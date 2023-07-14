a47c53c037a9000435f8f3461d08fdbe8d052c76
talha073 <talhajavedd@gmail.com>
2023-07-14 16:08:00 +0400

    contract and constructor setup

--- contracts/goDaddy.sol
+++ contracts/goDaddy.sol
@@ -6,11 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract goDaddy is ERC721 {
     uint256 public max_supply;
     uint256 public total_supply;
+    address owner;
 
     struct Domain {
         string name;
         uint256 cost;
-        address isOwner;
+        bool isOwner;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _supply) {
+        owner = msg.sender;
+    }
 }
