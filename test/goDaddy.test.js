3f164cadb3bdc6475a2b10f2f4dd91c162abe0bc
talha073 <talhajavedd@gmail.com>
2023-07-15 23:17:31 +0400

    test files setup

---
+++ test/goDaddy.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ethers");
+};
+
+describe("goDaddy", function () {
+  describe("Deployment", function () {
+    it("", async function () {});
+  });
+});
