8c61e76b01c8ef48990f0c876407ccf6dfd08f45
talha073 <talhajavedd@gmail.com>
2023-07-16 00:15:56 +0400

    deploy script

--- script/deplodgoDaddy.sol
+++ script/deplodgoDaddy.sol
@@ -1,19 +1,19 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.18;
-// import {Script} from "forge-std/Script.sol";
-// import {goDaddy} from "../src/goDaddy.sol";
-// // import {HelperConfig} from "./HelperConfig.s.sol";
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+import {goDaddy} from "../src/goDaddy.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 
-// contract DeploygoDaddy is Script {
-//     function run() external returns (FundMe) {
-//         HelperConfig helperConfig =  new HelperConfig();
-//         address ethUsdPriceFeed  = helperConfig.activeNetworkConfig();
-//         vm.startBroadcast();
-//         FundMe fundMe = new FundMe(ethUsdPriceFeed);
-//         vm.stopBroadcast();   
-//         return (fundMe);
-//     }
+contract DeploygoDaddy is Script {
+    function run() external returns (goDaddy) {
+        // HelperConfig helperConfig =  new HelperConfig();
+        // string memory ethUsdPriceFeed  = helperConfig.activeNetworkConfig();
+        vm.startBroadcast();
+        goDaddy godaddy = new goDaddy("goDaddy", "GG");
+        vm.stopBroadcast();   
+        return (godaddy);
+    }
     
-// }
+}
