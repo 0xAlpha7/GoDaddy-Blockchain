5fd6980845dfcc5ceddc2b4bfea7c59b74db3876
talha073 <talhajavedd@gmail.com>
2023-07-17 21:44:52 +0400

    test name and symbol

--- test/goDaddy.t.sol
+++ test/goDaddy.t.sol
@@ -6,16 +6,18 @@ import {goDaddy} from "../src/goDaddy.sol";
 
 contract CounterTest is Test {
     goDaddy godaddy;
-    string public name = "test";
-    string public symbol = "T";
+    string public name = "goDaddy";
+    string public symbol = "GG";
      
 
     function setUp() public {
         godaddy = new goDaddy(name, symbol);
     }
-    function testName() public {
-        string memory displayName = godaddy.name1();
-        assertEq(displayName, "Domain");
-        
+    function testNameAndSymbol() public {
+        string memory displayName = godaddy.name();
+        string memory displaySymbol = godaddy.symbol();
+        assertEq(displayName, name);
+        assertEq(displaySymbol, symbol);
+
     }
 }
